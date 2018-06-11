0c14429cf3704cf994f1f1ab577bd6821b963213
Nikhil Supekar <nikhilvsupekar@gmail.com>
2018-06-11 09:40:16 +0530

    partial Solution impl

--- src/com/utils/Solution.java
+++ src/com/utils/Solution.java
@@ -1,7 +1,23 @@
 package com.utils;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public interface Solution {
 
-    public void run();
+    default public void run(String className) {
+        String classDir = className.substring(4).replaceAll("\\.", "/");
+        String inputDir = "inputs/" + classDir;
+        String resultsDir = "results/" + classDir;
+        List<TestCase> testCases = TestCaseReader.readDirectory(inputDir);
 
+        List<Result> results = new ArrayList<>();
+        for (TestCase testCase : testCases) {
+            results.add(solve(testCase));
+        }
+
+        ResultWriter.writeDirectory(results, resultsDir);
+    }
+
+    Result solve(TestCase t);
 }
