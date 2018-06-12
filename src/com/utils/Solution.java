a886c03e87d871c2a9dc5bdf5b57574dc657de29
Nikhil Supekar <nikhilvsupekar@gmail.com>
2018-06-12 23:32:46 +0530

    Solution interface docs

--- src/com/utils/Solution.java
+++ src/com/utils/Solution.java
@@ -3,8 +3,28 @@ package com.utils;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * A general interface representing a Solution.
+ * Problems to solutions are implemented as objects of classes that implement the solution interface.
+ * The default run() takes care of most boiler plate code.
+ * The concrete solution to a problem must implement the solve method.
+ *
+ *
+ * @author  Nikhil Supekar
+ */
 public interface Solution {
 
+    /**
+     * Takes care of most of the boiler plate code that needs to be written by a solution provider.
+     * This includes reading
+     *      test cases from data/inputs/... directory
+     *      expected outputs from data/outputs/... directory
+     *      writing test results to data/results/... directory
+     * All these directories are determined at run time based on the name of the class calling this method.
+     * The method calls solve() on all test cases one-by-one and returns a list of ids of the failed test cases.
+     *
+     * @return List of Integers representing ids of failed test cases
+     */
     default List<Integer> run() {
         String className = this.getClass().getName();
         String classDir = className.substring(4).replaceAll("\\.", "/");
@@ -34,5 +54,15 @@ public interface Solution {
         return failedTestCaseIndices;
     }
 
+    /**
+     * This method triggers the execution of the solution.
+     * For best practices, the call to this method should be diverted to another method which does the actual computation.
+     * This ensures a smooth migration of the calling function to-and-fro the website.
+     * The input is a list of strings (list of lines) representing a typical test case.
+     * The interpretation of the input is problem-specific and needs to be parsed in the implementation.
+     *
+     * @param t     list of strings representing list of input lines as the test case
+     * @return      The output in the form of list of strings which is wrapped by FileContent class.
+     */
     FileContent solve(FileContent t);
 }
