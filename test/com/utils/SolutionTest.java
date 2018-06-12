545e60db63e559d432a9fb255bf5c6e1c666b83a
Nikhil Supekar <nikhilvsupekar@gmail.com>
2018-06-12 23:56:16 +0530

    SolutionTest dos

--- test/com/utils/SolutionTest.java
+++ test/com/utils/SolutionTest.java
@@ -1,13 +1,22 @@
 package com.utils;
 
-import org.junit.jupiter.api.Test;
-
 import java.util.ArrayList;
-
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * Abstract class defining the assertion to perform for all solutions.
+ * run() returns a list of failed test cases which is compared against an empty list.
+ * Tests for concrete solution classes must inherit this class and invoke testRun via the super construct.
+ *
+ * @author  Nikhil Supekar
+ */
 public abstract class SolutionTest {
 
+    /**
+     * Checks if the list of failed test cases is empty.
+     *
+     * @param solution  an instance of the concrete solution class
+     */
     public void testRun(Solution solution) {
         assertEquals(new ArrayList<>(), solution.run());
     }
